f3fd1f3edacad36556c724f897d9c1eaf2b00c50
vuthong <vuthong@luvina.net>
2022-02-14 17:09:55 +0700

    111

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -13,7 +13,7 @@ public class HelloGithub {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Github - 14/2/2022");
+		System.out.println("Hello Github");
 	}
 
 }
