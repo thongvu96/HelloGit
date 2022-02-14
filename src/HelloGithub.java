16542ef5f41dc5c66058ddb0c3277b0020a24b16
vuthong <vuthong@luvina.net>
2022-02-14 16:52:07 +0700

    Push lan dau

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -14,6 +14,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github - 14/2/2022");
+		System.out.println("Commit lan dau");
 	}
 
 }
