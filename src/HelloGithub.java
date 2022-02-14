5171a5869ec72c105d710bdaa0cc8a16e684f1bc
vuthong <vuthong@luvina.net>
2022-02-14 16:17:46 +0700

    Commit lần đầu

---
+++ src/HelloGithub.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author vuthong
+ *
+ */
+public class HelloGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github - 14/2/2022");
+	}
+
+}
