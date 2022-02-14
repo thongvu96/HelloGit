7af792a228c3cc4bad52db877410b3b33625ea53
vuthong <vuthong@luvina.net>
2022-02-14 16:55:25 +0700

    16h55 - 14/2/2022

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -14,7 +14,6 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github - 14/2/2022");
-		System.out.println("Commit lan dau");
 	}
 
 }
